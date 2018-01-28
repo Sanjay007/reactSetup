78fc517577bef56237520e0ef26f7e5444285e57
Sanjay <bqangarsanju12@gmail.com>
2018-01-28 18:42:17 +0530

    kkl

---
+++ Hello.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Hello extends React.Component {
+
+   render() {
+
+ function Sum(num) {
+  return num.first +num.second;
+}
+
+const number = {
+  first: 2,
+  second: 4
+};
+
+     const element=  <div>
+            <h1>Hello World from ReactJS</h1>
+            <hr/>
+            Yo Yo ! Welcome!
+         </div>
+         ;
+       return (
+         element
+     );
+     
+  
+   }
+}
+
+export default Hello;
